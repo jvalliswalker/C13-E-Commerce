47bb64e07c7adc166795c6f5d2e50772dd6ff5ae
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-17 21:09:03 -0700

    Create columns for product & tag, plus relationships via productTag

--- models/index.js
+++ models/index.js
@@ -6,12 +6,15 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category);
-Category.hasMany(Product);
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.hasMany(ProductTag)
+ProductTag.belongsTo(Product)
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany(ProductTag);
+ProductTag.belongsTo(Tag);
 
 module.exports = {
   Product,
