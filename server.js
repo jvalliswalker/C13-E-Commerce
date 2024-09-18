a56733ec019c86461104e0496d0e43bccf5d23ef
Jamil Vallis-Walker <jvalliswalker@gmail.com>
2024-09-17 19:26:24 -0700

    Add walkthrough comments

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const routes = require("./routes");
 const sequelize = require("sequelize");
-// import sequelize connection
 
+// Set up Express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Add middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Add routers
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// Sync sequelize and begin server listening 
 sequelize.sync({ force: false }).then(() =>
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
